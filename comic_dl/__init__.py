a82e2193ecb357a09140ec0041306531b66658b3
Dhruv Kanojia   (Xonshiz) <kanojia24.10@gmail.com>
2022-04-09 17:36:06 +0530

    Removing old code
    removing code

--- comic_dl/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# import sites
-# # import manga_eden
-# # import comic_dl
-# # import globalFunctions
-# # from comic_dl import ComicDL
--- comic_dl/__version__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__version__ = "2021.09.04.3"
